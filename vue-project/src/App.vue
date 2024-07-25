24d1fe2fa20a5048e09c215c7a11c3eed931b057
unknown <zelalemyohannes75@gmail.com>
2024-07-25 17:13:24 +0300

    commited

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,17 +2,20 @@
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
     <RouterView />
+
   </div>
 </template>
 
 <script>
 import SiteNavigation from "./components/SiteNavigation.vue";
+
 import { RouterView } from 'vue-router';
 
 export default {
   components: {
     SiteNavigation,
-    RouterView
+    RouterView,
+   
   }
 };
 </script>
